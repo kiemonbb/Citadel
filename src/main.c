94645fe280d28a26e71ca54470828866c89cb51a
Oktawian Bieszke <oktawianbieszke7@gmail.com>
2026-05-12 13:39:59 +0200

    test(i2c): update main to test i2c driver

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 #include "config.h"
 #include "hal/gpio_hal.h"
-#include "hal/spi_hal.h"
+#include "hal/i2c_hal.h"
 #include "stm32l031xx.h"
 #include "systick.h"
 int main(void) {
   systick_open();
-  spi_hal_open(SPI_CLOCK_MODE_0, SPI_BAUD_DIV_8);
+  i2c_hal_open();
 
-  uint8_t test[] = {0xA5, 0xDE, 0x55, 0xAA};
+  uint8_t test[] = {0xAA, 0xBB, 0xCC, 0xDD};
+  uint8_t recv_buffer[4] = {0};
 
+  int value = 1;
   while (1) {
-    spi_hal_cs_set(1);
-    spi_hal_write(test, 4, SPI_MAX_DELAY);
-    spi_hal_cs_set(0);
-    delay_ms(1000);
+    delay_ms(5000);
+    i2c_hal_reg(0x50, 0x00, test, sizeof(test), 0, 100);
+    delay_ms(100);
+    i2c_hal_reg(0x50, 0x00, recv_buffer, sizeof(recv_buffer), 1, 100);
   }
   return 0;
 }
